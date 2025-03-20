4c20c27a044710e6b84d4339d7e1a8a4723781c4
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-03-20 10:59:08 +0700

    membuat kondisi n tidak lebih dari 20 dan menampilkan pesan nya dan membuat jarak baris program

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -12,3 +12,12 @@ int arr[20; //membuat array dengan panjang data 20
         cin >> n; //memanggil variable inputan n
 
         if (n <= 20)
+        {//membuat kondisi n tidak lebih dari 20
+        break;
+        }
+        else
+        {
+        cout << "\nArray yang anda masukan maksimal 20 element.\n"; //menampilkan pesan jika data lebih dari 20
+        }
+        }
+        cout << endl; //membuat jarak per baris program
