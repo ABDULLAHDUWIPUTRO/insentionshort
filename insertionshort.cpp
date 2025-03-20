198f3e9246046d7addc6a96df3718e5191f2dfe5
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-03-20 10:55:12 +0700

    membuat inputan jumlah element array dan memanggil variable inputan n

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int arr[20; //membuat array dengan panjang data 20
     intn; //membuat variabel inputan n
+
+    void input()
+    {//procedure input
+    while (true)
+    {
+        cout << "masukan jumlah data pada array : "; //membuat inputan jumlah element array
+        cin >> n; //memanggil variable inputan n
+
+        if (n <= 20)
